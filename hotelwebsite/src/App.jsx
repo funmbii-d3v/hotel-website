a5584e8f6d607363ff1e9a95aa874fa85aabb21f
funmbii-d3v <adefunmbiadewemimo@gmail.com>
2026-08-19 15:01:01 +0100

    second commit

---
+++ hotelwebsite/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageRoute from './Routes/PageRoute'
+import "./Components/Styles/App.css"
+const App = () => {
+  return (
+    <div>
+      <PageRoute/>
+    </div>
+  )
+}
+
+export default App
---
+++ hotelwebsite/src/Routes/PageRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes, Router } from 'react-router-dom'
+import Navbar from '../Components/Navbar'
+const PageRoute = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default PageRoute
