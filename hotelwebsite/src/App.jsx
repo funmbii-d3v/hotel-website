e481fe5657bcc0400c81729caf7a182968af0fcb
funmbii-d3v <adefunmbiadewemimo@gmail.com>
2026-08-20 20:56:52 +0100

    commit

--- hotelwebsite/src/App.jsx
+++ hotelwebsite/src/App.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import PageRoute from './Routes/PageRoute'
 import "./Components/Styles/App.css"
+import styles from './Components/Styles/modal.module.css'
 const App = () => {
+  const [isOpen, setisOpen] = useState(true)
+  const navigate = useNavigate()
+
+  const toggleModal = () => {
+    setisOpen(!isOpen)
+  }
+  const goToRooms = () => {
+    setisOpen(false)
+    navigate('/rooms')
+  }
+
   return (
-    <div>
+    <div className='body'>
       <PageRoute/>
+      {isOpen &&(
+        <div className={styles.modals}>
+          <div className={styles.modalBox}>
+            <img src={"/Assets/image/Logo.png"} alt="hotel logo" height={150} width={150} />
+            <h1 className={styles.heading}>Hello, customer!</h1>
+            <h2 className={styles.text}>Welcome to L'Hòtel</h2>
+            <p className={styles.mini}>—home away from home✨</p>
+            <p>Looking to spend the night?</p>
+            <button onClick={goToRooms} className={styles.link}>Check out our rooms</button>
+            <button onClick={toggleModal} className={styles.cancel}>Maybe Later</button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
-
 export default App
--- hotelwebsite/src/Routes/PageRoute.jsx
+++ hotelwebsite/src/Routes/PageRoute.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Link, Route, Routes, Router } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import Navbar from '../Components/Navbar'
+import Home from "../Pages/Home.jsx"
+import Rooms from "../Pages/Rooms.jsx"
+
 const PageRoute = () => {
   return (
-    <div>
+    <>
       <Navbar/>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/rooms" element={<Rooms/>} />
+      </Routes>
+    </>
   )
 }
 
