e048709a2f4f052d0d7f3bffc96c06e7dc178e2b
thatguyloba <familugbamiracle@gmail.com>
2026-08-26 21:31:46 +0100

    lil change

--- hotelwebsite/src/Pages/About.jsx
+++ hotelwebsite/src/Pages/About.jsx
@@ -2,9 +2,76 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      
+  <React.Fragment>
+    <div className='Maincnt'>
+      <div className="container1">
+       <h1 className='head1'> Step Into Comfort</h1>
+       <button className='btn1'>Explore More</button>
+       <div className='links'>
+          
+       </div>
+      </div>
+      <div className="containers1">
+        <div className="ctop">
+          <p>Welcome To Our About Page</p>
+        <h1>L'Hotel B'n'B Amenities</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quibusdam vero illum debitis! Illum harum saepe ducimus libero soluta aperiam. <br></br> nostrum, omnis minus deleniti vero ad totam odio veniam veritatis.</p>
+        </div>
+          <div className="cbottom">
+            <div className="b1">
+              <h1>IC</h1>
+              <h1>Alarm</h1>
+            </div>
+            <div className="b2"> 
+              <h1>IC</h1>
+              <h1>Fast Wifi</h1>
+              </div>
+            <div className="b3"> 
+              <h1>IC</h1>
+              <h1>Bath</h1>
+              </div>
+            <div className="b4">
+               <h1>IC</h1>
+              <h1>Spa</h1>
+            </div>
+            <div className="b5">
+               <h1>IC</h1>
+              <h1>Coffe</h1>
+              </div>
+            <div className="b6">
+               <h1>IC</h1>
+              <h1>Safe</h1>
+            </div>
+       
+      </div>
+      </div>
+    
+      <div className="container2">
+        <div className="leftcont1">
+          <div className="box1"></div>
+          <div className="box2"></div>
+        </div>
+        <div className="rightcont1">
+          <div className="box3"></div>
+          <div className="box4"></div>
+        </div>
+      </div>
+      <div className="container3">
+        <div className="top1">
+          <h4>Feautures</h4>
+          <h1>Why Choose Us</h1>
+        </div>
+        <div className="bottom1">
+          <div className="c1"></div>
+          <div className="c2"></div>
+          <div className="c3"></div>
+          <div className="c4"></div>
+        </div>
+      </div>
+      <div className="container4"></div>
+      <div className="container5"></div>
     </div>
+  </React.Fragment>
   )
 }
 
